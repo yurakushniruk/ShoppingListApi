75d078a4572f0c390de88b2e35b999bcbf13a0c4
Yurii Kushniruk <yurakushniruk@ukr.net>
2022-10-31 20:53:16 +0100

    Initial

---
+++ Models/Grocery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ShoppingListApi.Models
+{
+    public class Grocery
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public Boolean Purchased { get; set; }
+
+        public Grocery()
+        {
+
+        }
+    }
+}
+
---
+++ Models/ShoppingListContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingListApi.Models
+{
+    public class ShoppingListContext : DbContext
+    {
+
+        public ShoppingListContext(DbContextOptions<ShoppingListContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Grocery> Grocery { get; set; }
+    }
+}
+
